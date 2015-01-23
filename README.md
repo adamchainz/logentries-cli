c371244d243f2c1bade016eced34c2815c89fd02
Adam Chainz <adam@adamj.eu>
2015-01-23 14:10:14 +0000

    Add LICENSE, README and setup.py

--- README.md
+++ README.md
@@ -1,6 +1,26 @@
 logentries-cli
 ==============
 
-Filter your logs on logentries direct on the comandline.
+Get your logs from Logentries on the comandline. Provides the `logentries`
+command which makes it easy to call their
+[https://logentries.com/doc/api-download/](Download API).
 
-Provides the `logentries` command.
+
+Examples
+--------
+
+`logentries web nginx`
+
+With only the required arguments (host and log) filled in, the last 20 minutes
+of messages from that log are downloaded (streamed through).
+
+`logentries web nginx -f code=500`
+
+Passing a [https://logentries.com/doc/search/](Logentries filter) means that
+fewer messages are downloaded and you can analyze faster.
+
+`logentries web nginx -f 'code=500' -s '1am' -e '5 minutes ago'`
+
+A wide variety of datetime formats are supported for specifying the start and
+end of the time period to fetch messages for, thanks to the awesome
+[https://github.com/bear/parsedatetime](parsedatetime).
---
+++ setup.py
@@ -0,0 +1,48 @@
+# -*- encoding:utf-8 -*-
+from __future__ import print_function
+import io
+
+from setuptools import setup
+
+
+def read(*filenames, **kwargs):
+    encoding = kwargs.get('encoding', 'utf-8')
+    sep = kwargs.get('sep', '\n')
+    buf = []
+    for filename in filenames:
+        with io.open(filename, encoding=encoding) as f:
+            buf.append(f.read())
+    return sep.join(buf)
+
+long_description = read('README.md')
+
+setup(
+    name='logentries-cli',
+    version='1.0',
+    description='Get your logs from Logentries on the comandline',
+    author='Adam Johnson',
+    author_email='me@adamj.eu',
+    url='http://github.com/adamchainz/logentries-cli/',
+    license='GPLv3',
+    install_requires=[
+        'parsedatetime>=1.4',
+        'python-dateutil>=2.4.0',
+        'requests>=2.5.1',
+    ],
+    scripts=['bin/logentries'],
+    long_description=long_description,
+    packages=[],
+    include_package_data=True,
+    platforms='any',
+    classifiers=[
+        'Programming Language :: Python',
+        'Development Status :: 4 - Beta',
+        'Natural Language :: English',
+        'Environment :: Console',
+        'Intended Audience :: Developers',
+        'Intended Audience :: System Administrators',
+        'License :: OSI Approved :: Apache Software License',
+        'Operating System :: OS Independent',
+        'Topic :: System :: Logging',
+    ],
+)
